0efcd7a751371f79b90e73402297ddaaf3807445
MartinHasi <martinhasieber@gmail.com>
2024-04-26 07:19:52 +0200

    updated index.html / style.css / script.js
    added button / Eventhandler / css

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 const containerDiv = document.querySelector('#container');
+const gridSizeBtn = document.querySelector('#getGridSize');
+let gridCreated = 0;
 
-
-function createDivs(rows, cols)
+function createDivGrid(rows)
 {
     for (let i = 0; i < rows; i++)
         for(let j = 0; j < rows; j++)
@@ -10,10 +11,24 @@ function createDivs(rows, cols)
             gridDiv.classList.add('gridBox');
             gridDiv.style.width = 100 / rows - .5+ '%';
             gridDiv.style.height = rows * 3 + 'px';
-            console.log(`Row: ${i} | Col: ${cols}`);
+            console.log(`Row: ${i} `);
             containerDiv.appendChild(gridDiv);
         }
 }
 
-
-createDivs(16, 16);
+function getGridSize()
+{
+    const gridSize = parseInt(prompt('How big should the Grid be? Max-Gridsize(100)'));
+    alert(gridSize);
+    if(gridSize >100)
+    {
+        alert('Grid Size is to big');   
+    }
+    else
+    {
+        containerDiv.innerText = '';
+        createDivGrid(gridSize);
+    }
+}
+gridSizeBtn.addEventListener('click', getGridSize);
+//createDivGrid(16);
